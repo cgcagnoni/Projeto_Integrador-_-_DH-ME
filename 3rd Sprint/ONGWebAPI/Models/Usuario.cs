fcf6e661e2a1b81f8b97f171a1a7057251ee0a9c
Lany Aragão <lanyaragao5@gmail.com>
2022-08-12 18:46:23 -0300

    Criando route api para o service email

--- 3rd Sprint/ONGWebAPI/Models/Usuario.cs
+++ 3rd Sprint/ONGWebAPI/Models/Usuario.cs
@@ -8,6 +8,7 @@ namespace ONGWebAPI.Models
         public string Nome { get; set; }
         public string Sobrenome { get; set; }
         public string? Localizacao { get; set; }
+        public string? Email { get; set; }
         public string? Telefone { get; set; }
         [JsonIgnore]
         public List<Animal>? Animais { get; set; }
