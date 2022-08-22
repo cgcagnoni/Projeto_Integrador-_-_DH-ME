0797b292de2ca6a63b23480148b13490915b6b76
Caroline-githubb <ingrid_caroliine@yahoo.com.br>
2022-08-21 22:23:18 -0300

    alterado para o padrao vue

---
+++ teste/associacao-das-patinhas-front/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Header from '@/components/Header/Header.vue'
+import Footer from '@/components/Footer/Footer.vue'
+</script>
+
+<template>
+  <div id="container">
+    <Header></Header>
+
+    <RouterView />
+
+    <Footer></Footer>
+  </div>
+</template>
