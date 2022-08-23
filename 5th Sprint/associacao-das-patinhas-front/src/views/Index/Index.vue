04c476f642b2c98dce76f6c61ae5bf99cfa4e619
lanyarag <lanyaragao5@gmail.com>
2022-08-23 18:13:30 -0300

    Implementação de media query nas pastas Vue

--- 5th Sprint/associacao-das-patinhas-front/src/views/Index/Index.vue
+++ 5th Sprint/associacao-das-patinhas-front/src/views/Index/Index.vue
@@ -1,31 +1,26 @@
 <template>
-  <div class="row homepage-princ" id="homepage">
-    <div class="col-6 homepage-img">
-      <img src="@/assets/images/Home/animal-2.jpg" alt="">
+<div class="row homepage-princ" id="homepage">
+      <div class="col homepage-img">
+        <img src="./assets/images/Home/animal-2.jpg" alt="">
+      </div>
+      <div class="col homepage-txt">
+        <span>
+          <h1 class=" homepage-txt-slog">Adotar é um ato de amor</h1>
+          <h2 class=" homepage-txt-nome">Associação das patinhas</h2>
+        </span>
+        <span class="homepage-btn">
+          <a href="./busca.html"><button class="homepage-btn-item">Adotar</button></a>
+          <a href="./arrecadacao.html"><button class="homepage-btn-item">Contribuir</button></a>
+          <a href="./cadastro.html"><button class="homepage-btn-item">Cadastro</button></a>
+        </span>
+      </div>
     </div>
-    <div class="col-6 homepage-txt">
-      <span>
-        <h1 class=" homepage-txt-slog">Adotar é um ato de amor</h1>
-        <h2 class=" homepage-txt-nome">Associação das patinhas</h2>
-      </span>
-      <span class="homepage-btn">
-        <!-- //precisa arrumar os botões -->
-        <RouterLink to="/busca"><button class="homepage-btn-item">Adotar</button></RouterLink>
-        <RouterLink to="/arrecadacao"><button class="homepage-btn-item">Contribuir</button></RouterLink>
-        <RouterLink to="/cadastro"><button class="homepage-btn-item">Cadastro</button></RouterLink>
-      </span>
-    </div>
-  </div>
 
-  <div id="homepage-catalog">
-    <div class="col homepage-catalog-txt">
-      <h3>Motivos para adotar</h3>
-      <div class="box">Você não participa do mercado de animais</div>
-      <div class="box">A adoção salva a vida de um animal</div>
-      <div class="box">Quem vive com pets tem menos estresse</div>
-    </div>
+    <!-- Catálogo da Home Page -->
     <div class="col homepage-catalog-carousel">
-      <h3>Animais disponíveis para adoção</h3>
+      <a href="/4th Sprint/busca.html">
+        <h3>Animais disponíveis para adoção</h3>
+      </a>
       <div class="carousel carousel-dark slide" data-bs-ride="carousel">
         <div class="carousel-indicators">
           <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active"
@@ -37,24 +32,21 @@
         </div>
         <div class="carousel-inner">
           <div class="carousel-item active" data-bs-interval="10000">
-            <img src="@/assets/images/Home/animal-5.jpg" class="d-block" alt="...">
+            <img src="./assets/images/Home/animal-5.jpg" class="d-block" alt="...">
             <div class="carousel-caption d-none d-md-block">
-              <p>Gato filhote</p>
               <p>Pedrinho</p>
             </div>
           </div>
           <div class="carousel-item" data-bs-interval="2000">
-            <img src="@/assets/images/Home/animal-3.jpg" class="d-block" alt="...">
+            <img src="./assets/images/Home/animal-4.jpg" class="d-block" alt="...">
             <div class="carousel-caption d-none d-md-block">
               <p>Lulu</p>
-              <p>Cadela porte médio</p>
             </div>
           </div>
           <div class="carousel-item">
-            <img src="@/assets/images/Home/animal-3.jpg" class="d-block" alt="...">
+            <img src="./assets/images/Home/animal-3.jpg" class="d-block" alt="...">
             <div class="carousel-caption d-none d-md-block">
               <p>Rajado</p>
-              <p>Cachorro adulto</p>
             </div>
           </div>
         </div>
@@ -68,7 +60,16 @@
         </button>
       </div>
     </div>
+    <div id="homepage-catalog row">
+      <div class="boxes">
+        <h3>Motivos para adotar</h3>
+        <div class="col box">Você não participa do mercado de animais</div>
+        <div class="col box">A adoção salva a vida de um animal</div>
+        <div class="col box">Quem vive com pets tem menos estresse</div>
+      </div>
+    </div>
   </div>
+
 </template>
 
 <script src="./Index.js"></script>
