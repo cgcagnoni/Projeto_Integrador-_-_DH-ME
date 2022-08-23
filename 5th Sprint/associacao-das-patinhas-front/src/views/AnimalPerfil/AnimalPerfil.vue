3222927ec08a3e8bd349058071db780920b42b73
lanyarag <lanyaragao5@gmail.com>
2022-08-23 18:56:15 -0300

    Adaptação do código ao vue

--- 5th Sprint/associacao-das-patinhas-front/src/views/AnimalPerfil/AnimalPerfil.vue
+++ 5th Sprint/associacao-das-patinhas-front/src/views/AnimalPerfil/AnimalPerfil.vue
@@ -1,62 +1,61 @@
 <template>
-    <article>
-        <div id="animal-perfil" class="row animal-perfil-princ">
-            <!-- INFOS PROVISORIAS, TROCAR PELA API DEPOIS -->
-            <div class="col-5 animal-perfil-img">
-                <img src="@/assets/images/PaginaAnimal/animal-5.jpg" alt="imagem-animal-pretendido">
+  <article>
+    <div id="animal-perfil" class="animal-perfil-princ">
+      <!-- INFOS PROVISORIAS, TROCAR PELA API DEPOIS -->
+      <div class="animal-perfil-img">
+        <img
+          src="@/assets/images/PaginaAnimal/animal-5.jpg"
+          alt="imagem-animal-pretendido"
+        />
 
-                <div class="col-5 tags-filtro">
-                    <!--linkar aqui com a api as tags que estao registradas no animal -->
-                    #macho
-                    #dócil
-                    #filhote
-                </div>
-            </div>
-
-            <div class="col-7 descricao-animal">
-                <h1 class="nome-animal"> PEDRINHO </h1>
-                <p class="animal-txt"> Pedrinho veio de uma ninhada com outros seis gatinhos, encontrados
-                    abandonados em
-                    um terreno.
-                    Tem o pelo rajadinho e os olhos azuis, muito dócil e carinhoso. Aparenta ter em torno de 4 a
-                    5 meses
-                    e está pronto para ter um lar!
-                </p>
-
-                <!-- redirecionar para modal alert -->
-                <div class="adocao-btn">
-                    <a href=""><button class="adocao-btn-item">VOU ADOTAR!</button></a>
-                </div>
-            </div>
+        <div class="tags-filtro">
+          <!--linkar aqui com a api as tags que estao registradas no animal -->
+          #macho #dócil #filhote
         </div>
-    </article>
+      </div>
 
-    <aside>
-        <div class="sugestoes">
-            <h2>Outros peludinhos que também precisam de um lar</h2>
+      <div class="descricao-animal">
+        <h1 class="nome-animal">PEDRINHO</h1>
+        <p class="animal-txt">
+          Pedrinho veio de uma ninhada com outros seis gatinhos, encontrados
+          abandonados em um terreno. Tem o pelo rajadinho e os olhos azuis,
+          muito dócil e carinhoso. Aparenta ter em torno de 4 a 5 meses e está
+          pronto para ter um lar!
+        </p>
 
-            <div class="outros-animais">
-                <RouterLink to="/busca" target="_blank">
-                    <img src="@/assets/images/PaginaAnimal/animal-1.jpg" alt="">
-                    <p>Lulu</p>
-                </RouterLink>
-                <RouterLink to="/busca" target="_blank">
-                    <img src="@/assets/images/PaginaAnimal/animal-6.jpg" alt="">
-                    <p>Rajado</p>
-                </RouterLink>
-                <RouterLink to="/busca" target="_blank">
-                    <img src="@/assets/images/PaginaAnimal/animal-7.jpg" alt="">
-                    <p>Gigante</p>
-                </RouterLink>
-                <RouterLink to="/busca" target="_blank">
-                    <img src="@/assets/images/PaginaAnimal/animal-8.jpg" alt="">
-                    <p>Soneca</p>
-                </RouterLink>                  
-            </div>
+        <!-- redirecionar para modal alert -->
+        <div class="adocao-btn">
+          <a href=""><button class="adocao-btn-item">VOU ADOTAR!</button></a>
         </div>
-    </aside>
+      </div>
+    </div>
+  </article>
+
+  <aside>
+    <div class="sugestoes">
+      <h2>Outros peludinhos que também precisam de um lar</h2>
+
+      <div class="outros-animais">
+        <RouterLink to="/busca" target="_blank">
+          <img src="@/assets/images/PaginaAnimal/animal-1.jpg" alt="" />
+          <p>Lulu</p>
+        </RouterLink>
+        <RouterLink to="/busca" target="_blank">
+          <img src="@/assets/images/PaginaAnimal/animal-6.jpg" alt="" />
+          <p>Rajado</p>
+        </RouterLink>
+        <RouterLink to="/busca" target="_blank">
+          <img src="@/assets/images/PaginaAnimal/animal-7.jpg" alt="" />
+          <p>Gigante</p>
+        </RouterLink>
+        <RouterLink to="/busca" target="_blank">
+          <img src="@/assets/images/PaginaAnimal/animal-8.jpg" alt="" />
+          <p>Soneca</p>
+        </RouterLink>
+      </div>
+    </div>
+  </aside>
 </template>
 
 <script src="./AnimalPerfil.js"></script>
-<style src="./AnimalPerfil.css">
-</style>
+<style src="./AnimalPerfil.css"></style>
--- 5th Sprint/associacao-das-patinhas-front/src/views/Index/Index.vue
+++ 5th Sprint/associacao-das-patinhas-front/src/views/Index/Index.vue
@@ -1,19 +1,24 @@
 <template>
-<div class="row homepage-princ" id="homepage">
-      <div class="col homepage-img">
-        <img src="./assets/images/Home/animal-2.jpg" alt="">
-      </div>
-      <div class="col homepage-txt">
-        <span>
-          <h1 class=" homepage-txt-slog">Adotar é um ato de amor</h1>
-          <h2 class=" homepage-txt-nome">Associação das patinhas</h2>
-        </span>
-        <span class="homepage-btn">
-          <a href="./busca.html"><button class="homepage-btn-item">Adotar</button></a>
-          <a href="./arrecadacao.html"><button class="homepage-btn-item">Contribuir</button></a>
-          <a href="./cadastro.html"><button class="homepage-btn-item">Cadastro</button></a>
-        </span>
-      </div>
+  <div class="row homepage-princ" id="homepage">
+    <div class="col homepage-img">
+      <img src="@/assets/images/Home/animal-2.jpg" alt="" />
+    </div>
+    <div class="col homepage-txt">
+      <span>
+        <h1 class="homepage-txt-slog">Adotar é um ato de amor</h1>
+        <h2 class="homepage-txt-nome">Associação das patinhas</h2>
+      </span>
+      <span class="homepage-btn">
+        <RouterLink to="/busca"
+          ><button class="homepage-btn-item">Adotar</button></RouterLink
+        >
+        <RouterLink to="/arrecadacao"
+          ><button class="homepage-btn-item">Contribuir</button></RouterLink
+        >
+        <RouterLink to="/cadastro"
+          ><button class="homepage-btn-item">Cadastro</button></RouterLink
+        >
+      </span>
     </div>
 
     <!-- Catálogo da Home Page -->
@@ -23,38 +28,74 @@
       </a>
       <div class="carousel carousel-dark slide" data-bs-ride="carousel">
         <div class="carousel-indicators">
-          <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active"
-            aria-current="true" aria-label="Slide 1"></button>
-          <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1"
-            aria-label="Slide 2"></button>
-          <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2"
-            aria-label="Slide 3"></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleDark"
+            data-bs-slide-to="0"
+            class="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleDark"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleDark"
+            data-bs-slide-to="2"
+            aria-label="Slide 3"
+          ></button>
         </div>
         <div class="carousel-inner">
           <div class="carousel-item active" data-bs-interval="10000">
-            <img src="./assets/images/Home/animal-5.jpg" class="d-block" alt="...">
+            <img
+              src="@/assets/images/Home/animal-5.jpg"
+              class="d-block"
+              alt="..."
+            />
             <div class="carousel-caption d-none d-md-block">
               <p>Pedrinho</p>
             </div>
           </div>
           <div class="carousel-item" data-bs-interval="2000">
-            <img src="./assets/images/Home/animal-4.jpg" class="d-block" alt="...">
+            <img
+              src="@/assets/images/Home/animal-4.jpg"
+              class="d-block"
+              alt="..."
+            />
             <div class="carousel-caption d-none d-md-block">
               <p>Lulu</p>
             </div>
           </div>
           <div class="carousel-item">
-            <img src="./assets/images/Home/animal-3.jpg" class="d-block" alt="...">
+            <img
+              src="@/assets/images/Home/animal-3.jpg"
+              class="d-block"
+              alt="..."
+            />
             <div class="carousel-caption d-none d-md-block">
               <p>Rajado</p>
             </div>
           </div>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+        <button
+          class="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleDark"
+          data-bs-slide="prev"
+        >
           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
           <span class="visually-hidden">Previous</span>
         </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+        <button
+          class="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleDark"
+          data-bs-slide="next"
+        >
           <span class="carousel-control-next-icon" aria-hidden="true"></span>
           <span class="visually-hidden">Next</span>
         </button>
@@ -69,9 +110,7 @@
       </div>
     </div>
   </div>
-
 </template>
 
 <script src="./Index.js"></script>
-<style src="./Index.css">
-</style>
+<style src="./Index.css"></style>
