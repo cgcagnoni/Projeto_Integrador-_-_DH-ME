276416ec009887c1b7226710566e1522d88f2f85
Jaqueline Rossetto <jaquerossetto@hotmail.com>
2022-08-25 16:00:24 -0300

    adicoes no media querry

--- 5th Sprint/associacao-das-patinhas-front/src/views/DoacaoAnimal/DoacaoAnimal.vue
+++ 5th Sprint/associacao-das-patinhas-front/src/views/DoacaoAnimal/DoacaoAnimal.vue
@@ -1,8 +1,120 @@
 <template>
   <!-- Conteúdo da Página -->
+  <div id="form_doacao">
+    <div id="img_form_doacao">
+      <img src="@/assets/images/DoacaoAnimal/cão-e-gato.jpg" />
+    </div>
+    <div id="forms_doacao">
+      <form action="#">
+        <div id="titulo_forms_doacao">
+          <h1>Cadastre o seu animal</h1>
+          <hr class="solid" />
+        </div>
+        <div class="form-header_doacao">
+          <div class="input-group">
+            <div class="input-box">
+              <label for="nome">Nome :</label>
+              <div>
+                <input
+                  required
+                  type="text"
+                  placeholder="Digite o nome do seu pet"
+                />
+              </div>
+            </div>
+            <div class="input-box">
+              <label for="idade">Idade :</label>
+              <div>
+                <input
+                  type="text"
+                  required
+                  placeholder="Digite a idade do seu pet"
+                />
+              </div>
+            </div>
+            <div class="input-select_porte">
+              <label for="porte">Porte :</label>
+              <div>
+                <select name="porte" id="porte">
+                  <option value="" block>Selecione o porte</option>
+                  <option value="Pequeno">Pequeno</option>
+                  <option value="Médio">Médio</option>
+                  <option value="Grande">Grande</option>
+                </select>
+              </div>
+            </div>
+            <div class="inputs-radio_doacao">
+              <div class="input-radio">
+                <h6>Especie :</h6>
+                <div>
+                  <input type="radio" name="especie" value="Gato" />
+                  <label for="Gato">Gato</label>
+                  <input type="radio" name="espeie" value="Cachorro" /><label
+                    for="Cachorro"
+                    >Cachorro</label
+                  >
+                </div>
+              </div>
+            </div>
+            <div class="input-box">
+              <label for="Foto"> Adicione uma foto do seu pet</label>
+              <div>
+                <input for="Foto" type="file" placeholder="Adicione a Foto" />
+              </div>
+            </div>
+            <div class="inputs-radio_doacao">
+              <h6>Sexo :</h6>
+              <div>
+                <input type="radio" name="sexo" value="Macho" />
+                <label for="Macho">Macho</label>
+                <input type="radio" name="sexo" value="Fêmea" />
+                <label for="Fêmea">Fêmea</label>
+              </div>
+            </div>
+            <div class="input-checkbox">
+              <input type="checkbox" name="vacina" />
+              <label for="vacina" class="label-check">
+                Seu pet é vacinado?</label
+              >
+            </div>
+            <div class="input-checkbox">
+              <input type="checkbox" name="Microchip" />
+              <label for="Microchip" class="label-check">
+                Seu pet possui Microchip?</label
+              >
+            </div>
+            <div class="input-checkbox">
+              <input type="checkbox" name="Castrado" />
+              <label for="Castrado" class="label-check">
+                Seu pet é castrado?</label
+              >
+            </div>
+            <div class="input-checkbox">
+              <input type="checkbox" name="deficiencia" />
+              <label for="deficiencia" class="label-check">
+                Seu pet possui alguma deficiencia?</label
+              >
+            </div>
+
+            <div class="Cadastre-se-button">
+              <button><a href="#"> Salvar</a></button>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script src="./DoacaoAnimal.js"></script>
+<style src="./DoacaoAnimal.css"></style>
+
+<!-- backup caso precise para outros metodos 
+
+<template>
   <div id="form">
     <div id="img_form">
-      <img src="@/assets/images/DoacaoAnimal/cão-e-gato.jpg">
+      <img src="@/assets/images/DoacaoAnimal/cão-e-gato.jpg" />
     </div>
     <div id="forms">
       <form action="#">
@@ -13,11 +125,19 @@
           <div class="input-group">
             <div class="input-box">
               <label for="nome">Nome :</label>
-              <input requeired type="text" placeholder="Digite o nome do seu pet">
+              <input
+                requeired
+                type="text"
+                placeholder="Digite o nome do seu pet"
+              />
             </div>
             <div class="input-box">
               <label for="idade">Idade :</label>
-              <input type="text" required placeholder="Digite a idade do seu pet">
+              <input
+                type="text"
+                required
+                placeholder="Digite a idade do seu pet"
+              />
             </div>
             <div class="input-select">
               <label for="porte">Porte :</label>
@@ -31,35 +151,48 @@
             <div class="inputs-radio">
               <div class="input-radio">
                 <h6>Especie :</h6>
-                <input type="radio" name="espeie" value="Gato"> <label for="Gato">Gato</label>
-                <input type="radio" name="espeie" value="Cachorro"><label for="Cachorro">Cachorro</label>
-
+                <input type="radio" name="espeie" value="Gato" />
+                <label for="Gato">Gato</label>
+                <input type="radio" name="espeie" value="Cachorro" /><label
+                  for="Cachorro"
+                  >Cachorro</label
+                >
               </div>
             </div>
             <div class="input-box">
               <label for="Foto"> Adicione uma foto do seu pet</label>
-              <input for="Foto" type="file" placeholder="Adicione a Foto">
+              <input for="Foto" type="file" placeholder="Adicione a Foto" />
             </div>
             <div class="input-radio">
               <h6>Sexo :</h6>
-              <input type="radio" name="sexo" value="Macho"> <label for="Macho">Macho</label>
-              <input type="radio" name="sexo" value="Fêmea"> <label for="Fêmea">Fêmea</label>
+              <input type="radio" name="sexo" value="Macho" />
+              <label for="Macho">Macho</label>
+              <input type="radio" name="sexo" value="Fêmea" />
+              <label for="Fêmea">Fêmea</label>
             </div>
             <div class="input-checkbox">
-              <input type="checkbox" name="vacina">
-              <label for="vacina" class="label-check"> Seu pet é vacinado?</label>
+              <input type="checkbox" name="vacina" />
+              <label for="vacina" class="label-check">
+                Seu pet é vacinado?</label
+              >
             </div>
             <div class="input-checkbox">
-              <input type="checkbox" name="Microchip">
-              <label for="Microchip" class="label-check"> Seu pet possui Microchip?</label>
+              <input type="checkbox" name="Microchip" />
+              <label for="Microchip" class="label-check">
+                Seu pet possui Microchip?</label
+              >
             </div>
             <div class="input-checkbox">
-              <input type="checkbox" name="Castrado">
-              <label for="Castrado" class="label-check"> Seu pet é castrado?</label>
+              <input type="checkbox" name="Castrado" />
+              <label for="Castrado" class="label-check">
+                Seu pet é castrado?</label
+              >
             </div>
             <div class="input-checkbox">
-              <input type="checkbox" name="deficiencia">
-              <label for="deficiencia" class="label-check"> Seu pet possui alguma deficiencia?</label>
+              <input type="checkbox" name="deficiencia" />
+              <label for="deficiencia" class="label-check">
+                Seu pet possui alguma deficiencia?</label
+              >
             </div>
 
             <div class="Cadastre-se-button">
@@ -70,10 +203,5 @@
       </form>
     </div>
   </div>
-
-
 </template>
-
-  <script src="./DoacaoAnimal.js"></script>
-    <style src="./DoacaoAnimal.css">
-    </style>
+-->
