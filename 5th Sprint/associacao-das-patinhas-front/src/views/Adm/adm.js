a90e308296655f131c3556d442d32d778516c5cc
Caroliny S Q G Cagnoni <cgcagnoni@gmail.com>
2022-08-25 11:52:03 -0300

    corrigido bug UsuarioController.cs e adicionado listaUsuario

--- 5th Sprint/associacao-das-patinhas-front/src/views/Adm/adm.js
+++ 5th Sprint/associacao-das-patinhas-front/src/views/Adm/adm.js
@@ -1,8 +1,56 @@
 export default {
+    el: "#list-user",
+
     data() {
         return {
+            id: 0,
+            username: null,
+            role: 0,
+            password: null,
+            nome: null,
+            sobrenome: null,
+            localizacao: null,
+            email: null,
+            telefone: null,
+            autorizacaoNotificacao: 0,
+            listaUsuarios: [
+
+            ]
         };
+
     },
+
     methods: {
-    },
+
+        listarTodosUsuarios() {
+            
+            const headers = new Headers();
+            const token = localStorage.getItem("token")
+
+            if (token) {
+                headers.append("Authorization", `Bearer ${token}`)
+            }
+
+            let app = { method: 'GET',
+               headers };
+
+            fetch(`https://localhost:7288/api/Usuario`, app).then(resp => {
+                resp.json().then(usuarios => {
+                    this.listaUsuarios=usuarios;
+                });
+             })
+        },
+
+        beforeMount() {   
+            
+            const token = localStorage.getItem("token")
+
+            if (token) {            
+                this.listarTodosUsuarios();
+            }             
+       }   
+       
+
+    }
+    
 }
--- 5th Sprint/associacao-das-patinhas-front/src/views/Adm/adm.vue
+++ 5th Sprint/associacao-das-patinhas-front/src/views/Adm/adm.vue
@@ -5,69 +5,67 @@
                 <div class="col">
                     <div class="adm-anim">
                         <h1>Animais</h1>
-                        <RouterLink to="/alteracao-animal">
-                            <button>
-                                Alteração de cadastro
-                            </button>
-                        </RouterLink>
-                        <RouterLink to="/busca">
-                            <button>
+                        <button> <a href="/alteracao-animal">
+                         Alteração de cadastro       
+                            </a></button>
+                        <button>
+                            <a href="/busca">
                                 Buscar um animal
-                            </button>
-                        </RouterLink>
-                        <RouterLink to="#">
-                            <button type="button" v-on:click="criarListaAnimais">
-
+                            </a></button>
+                        <button type="button" v-on:click="criarListaAnimais">
+                            <a href="#">
                                 Lista de animais
-                            </button>
-                        </RouterLink>
-                        <RouterLink to="#">
-                            <button>
-
+                            </a></button>
+                        <button>
+                            <a href="#">
                                 Deletar um animal
-                            </button>
-                        </RouterLink>
+                            </a></button>
                     </div>
                 </div>
                 <div class="col">
                     <div class="adm-user">
                         <h1>Usuários</h1>
-                        <RouterLink to="/alteracao-cadastro">
-                            <button>
-
+                        <button>
+                            <a href="alteracao-cadastro">
                                 Alteração de cadastro
-                            </button>
-                        </RouterLink>
-                        <RouterLink to="#">
-                            <button>
-
+                            </a></button>
+                        <button v-on:click="listarTodosUsuarios()">
                                 Lista de usuários
-                            </button>
-                        </RouterLink>
-                        <RouterLink to="#">
-                            <button>
-
+                        </button>
+                        <button>
+                            <a href="#">
                                 Deletar um usuário
-                            </button>
-                        </RouterLink>
+                            </a>
+                        </button>
                     </div>
                 </div>
                 <div class="col">
                     <div class="adm-ong">
                         <h1>Administração</h1>
-                        <RouterLink to="#">
                         <button>
-                           
+                            <a RouterLink="/4th Sprint/alteracao-de-cadastro.html">
                                 Doações
-                            </button>
-                            </RouterLink>
+                            </a></button>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    <div id="listas">
+        <ul id="list-user">
+            
+            <li class="list-group" v-for="usuario in listaUsuarios">
+            <h1 id="listaDeUsers">Usuário {{ usuario.id }}</h1><br/>
+                Username: {{ usuario.username }} <br/>
+                Nome: {{ usuario.nome }}<br/>
+                Sobrenome: {{ usuario.sobrenome }}<br/>
+                Localizacao: {{ usuario.localizacao }}<br/>
+                E-mail: {{ usuario.email }}<br/>
+                Telefone: {{ usuario.telefone }}<hr>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script src="./adm.js"></script>
-<style src="./adm.css">
-</style>
+<style src="./adm.css"></style>
