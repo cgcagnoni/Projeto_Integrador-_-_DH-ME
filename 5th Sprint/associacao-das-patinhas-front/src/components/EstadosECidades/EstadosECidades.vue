89e634b8389da64c8ebd3716d2de4957efaf1009
Caroline-githubb <ingrid_caroliine@yahoo.com.br>
2022-08-26 20:39:51 -0300

    Componente EstadosECidades

---
+++ 5th Sprint/associacao-das-patinhas-front/src/components/EstadosECidades/EstadosECidades.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="estados" class="input-box">
+        <label class="form-label" for="localizacao">Estado</label>
+        <select id="UF" name="UF" v-model="estado">
+            <option value="">Selecione</option>
+            <option :value="item" v-for="item in listaUF">{{ item }}</option>
+        </select>
+    </div>
+    <div class="input-box">
+        <label class="form-label" for="localizacao">Cidade</label>
+        <select id="UF" name="UF" v-model="cidade">
+            <option value="">Selecione</option>
+            <option :value="item" v-for="item in listaCidades[estado]">{{item}}</option>
+        </select>
+    </div>
+</template>
+
+<script src="./EstadosECidades.js"></script>
