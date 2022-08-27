b65541ef8ededfc0967589733f879210f71f04b4
Lany Aragão <lanyaragao5@gmail.com>
2022-08-27 16:21:18 -0300

    Página de busca com filtros

--- 5th Sprint/associacao-das-patinhas-front/src/views/Busca/Busca.vue
+++ 5th Sprint/associacao-das-patinhas-front/src/views/Busca/Busca.vue
@@ -1,28 +1,93 @@
 <template>
-  <!-- Conteúdo da Página -->
-
+  <!-- Filtros -->
   <div id="busca">
-    <div class="container-busca text-center row">
-      <div class="col-11 input-busca">
-        <input
-          type="text"
-          class="form-control"
-          id="busca-input"
-          placeholder="Busque por porte, idade ou sexo do animal"
-        />
-      </div>
-      <div class="col-1">
-        <p for="busca-input" class="busca-icon">
-          <i class="fa-solid fa-magnifying-glass fa-2x"></i>
-        </p>
+    <div class="form-group row">
+      <label for="alterarEspecie">
+        <h2 class="tituloAnimal">Espécie: </h2>
+      </label>
+      <div class="tituloAnimal-inputs col-sm-10">
+        <input v-model="especie" type="radio" name="especie" value="0">
+        <label for="Gato">Gato</label><br>
+        <input v-model='especie' type="radio" name="especie" value="1">
+        <label for="Cachorro">Cachorro</label>
       </div>
     </div>
+    <hr>
+
+    <div class="form-group row">
+      <label for="alterarIdade">
+        <h2 class="tituloAnimal">Idade: </h2>
+      </label>
+      <div class="col-sm-10">
+        <input v-model="idade" type="radio" name="idade" value="0">
+        <label for="Filhote">Filhote</label><br />
+        <input v-model="idade" type="radio" name="idade" value="1">
+        <label for="Adulto">Adulto</label><br />
+        <input v-model="idade" type="radio" name="idade" value="2">
+        <label for="Idoso">Idoso</label><br />
+      </div>
+    </div>
+    <hr>
+     <div class="form-group row">
+      <label for="alterarEspecie">
+        <h2 class="tituloAnimal">Sexo: </h2>
+      </label>
+      <div class="tituloAnimal-inputs col-sm-10">
+        <input v-model="sexo" type="radio" name="sexo" value="0">
+        <label for="Macho">Macho</label><br>
+        <input v-model="sexo" type="radio" name="sexo" value="1">
+        <label for="Fêmea">Fêmea</label>
+      </div>
+    </div>
+    <hr>
+
+    <div class="form-group row">
+      <label for="alterarPorte">
+        <h2 class="tituloAnimal">Porte: </h2>
+      </label>
+      <div class="col-sm-10">
+        <input v-model="porte" type="radio" name="porte" value="0">
+        <label for="Pequeno">Pequeno</label><br />
+        <input v-model="porte" type="radio" name="porte" value="1">
+        <label for="Médio">Médio</label><br />
+        <input v-model="porte" type="radio" name="porte" value="2">
+        <label for="Grande">Grande</label><br />
+      </div>
+    </div>
+    <hr>
+  <div class="btn-busca">
+    <button v-on:click="filtrarAnimais">
+      Buscar animal
+    </button>
+  </div>
   </div>
 
-  <!-- Catálogo -->
-  <div id="catalog"></div>
 
-  <!-- Criar paginação -->
+
+  <!-- Catálogo -->
+  <div class="title-busca">
+   <h1>Animais disponíveis para adoção</h1>
+  </div>
+    <div id="catalog" v-for="animal in this.animaisFiltrados">
+        <div v-if="animal.especie==0" class="card">
+            <img src="https://cataas.com/cat" alt="imagem-animal"/>
+            <div class="cardanimal">
+                <RouterLink :to="{ name: 'animalperfil', params: { id: animal.id } }">
+                    <h2>{{ animal.nome }}</h2>
+                  </RouterLink>
+            </div>
+        </div>
+        <div v-if="animal.especie==1" class="card">
+            <img src="https://placedog.net/640/480?random" alt="imagem-animal"/>
+            <div class="cardanimal">
+                <RouterLink :to="{ name: 'animalperfil', params: { id: animal.id } }">
+                    <h2>{{ animal.nome }}</h2>
+                  </RouterLink>
+            </div>
+        </div>
+    </div>
+
+  <!-- Paginação -->
   <div id="pag">
     <nav>
       <ul class="pagination">
@@ -45,4 +110,5 @@
 </template>
 
 <script src="./Busca.js"></script>
-<style src="./Busca.css"></style>
+<style src="./Busca.css">
+</style>
